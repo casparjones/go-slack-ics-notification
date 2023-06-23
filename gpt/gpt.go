7a6e3fa6c5b53af577a527c38efe4ba97ad60a6a
Frank Vlatten <frank@vlatten.biz>
2023-06-23 19:44:43 +0200

    adding a context to the conversation for the channel

--- gpt/gpt.go
+++ gpt/gpt.go
@@ -49,6 +49,28 @@ func (c *Chat) SendAsync(message Event) chan slack.Response {
 	return rchan
 }
 
+func (c *Chat) AddMessageToConversation(conversationId string, message Message) {
+	if conversationStorage == nil {
+		conversationStorage = make(map[string][]Message)
+	}
+
+	if len(conversationStorage[conversationId]) > 20 {
+		conversationStorage[conversationId] = []Message{}
+	}
+	conversationStorage[conversationId] = append(conversationStorage[conversationId], message)
+}
+
+func (c *Chat) GetMessageInConversation(conversationId string) []Message {
+	defaultSystemMessages := []Message{
+		{
+			Role:    "system",
+			Content: "Your name ist lovelyapps-Bot and you are a female helpful assistant. If something you ask if you male or female, then say: female. You know the apps langify, geolizr and shopify. langify is a Shopify app for translations and geolizr is a geo-based event app also for Shopify. Shopify is a Canadian store system for which we develop apps.",
+		},
+	}
+
+	return append(defaultSystemMessages, conversationStorage[conversationId]...)
+}
+
 func (c *Chat) Send(event Event, responseChan chan slack.Response) slack.Response {
 	response := slack.Instance.SendMessage(event.Channel, event.User, slack.GetSimpleMessage(event.User, event.Channel, "... thinking ..."))
 	go func() {
@@ -57,18 +79,15 @@ func (c *Chat) Send(event Event, responseChan chan slack.Response) slack.Respons
 
 	event.Timestamp = response.Ts
 
+	c.AddMessageToConversation(event.Channel, Message{
+		Role:    "user",
+		Content: event.Text,
+	})
+
+	messages := c.GetMessageInConversation(event.Channel)
 	data := Data{
-		Model: "gpt-3.5-turbo",
-		Messages: []Message{
-			{
-				Role:    "system",
-				Content: "Your name ist lovelyapps-Bot and you are a female helpful assistant. If something you ask if you male or female, then say: female. You know the apps langify, geolizr and shopify. langify is a Shopify app for translations and geolizr is a geo-based event app also for Shopify. Shopify is a Canadian store system for which we develop apps.",
-			},
-			{
-				Role:    "user",
-				Content: event.Text,
-			},
-		},
+		Model:    "gpt-3.5-turbo",
+		Messages: messages,
 	}
 
 	dataStr, err := json.Marshal(data)
@@ -86,7 +105,18 @@ func (c *Chat) Send(event Event, responseChan chan slack.Response) slack.Respons
 	}
 
 	gptResponseString := resp.String()
-	slackMessage := c.returnSlackMessage(gptResponseString)
+	var gptResponse GptResponse
+	err = json.Unmarshal([]byte(gptResponseString), &gptResponse)
+	if err != nil {
+		log.Fatalf("Error occurred while unmarshalling GPT-3 response: %v", err)
+	}
+
+	c.AddMessageToConversation(event.Channel, Message{
+		Role:    "assistant",
+		Content: gptResponse.Choices[0].Message.Content,
+	})
+
+	slackMessage := c.returnSlackMessage(gptResponse)
 
 	if event.Timestamp == "" {
 		return slack.Instance.SendMessage(event.Channel, event.User, slackMessage)
@@ -96,15 +126,9 @@ func (c *Chat) Send(event Event, responseChan chan slack.Response) slack.Respons
 
 }
 
-func (c *Chat) returnSlackMessage(gptResponse string) slack.Message {
-	var response GptResponse
-	err := json.Unmarshal([]byte(gptResponse), &response)
-	if err != nil {
-		log.Fatalf("Error occurred while unmarshalling GPT-3 response: %v", err)
-	}
-
+func (c *Chat) returnSlackMessage(gptResponse GptResponse) slack.Message {
 	// Implement your logic to transform the gptResponse into a SlackMessage.
-	answer := response.Choices[0].Message.Content
+	answer := gptResponse.Choices[0].Message.Content
 	text := slack.Text{
 		Type: "mrkdwn",
 		Text: answer,
@@ -120,6 +144,8 @@ func (c *Chat) returnSlackMessage(gptResponse string) slack.Message {
 		Blocks: []slack.Block{block},
 	}
 }
+
+var conversationStorage map[string][]Message
 
 func NewChat() Chat {
 	chat := Chat{
